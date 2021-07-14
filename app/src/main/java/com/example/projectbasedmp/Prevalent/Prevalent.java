d9bda74aa5b1f8c538053d2b8b37c5fe648f0133
Afiffuddin Noli <87260297+afiffddn@users.noreply.github.com>
2021-07-14 16:14:02 +0800

    Initial commit
    First commit

---
+++ app/src/main/java/com/example/projectbasedmp/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.projectbasedmp.Prevalent;
+
+import com.example.projectbasedmp.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
